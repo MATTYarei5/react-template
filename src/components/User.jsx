04625be29bf24f4b16905cb68bf718cb3c469d64
Matt Nebeker <mattn@devpipeline.com>
2024-08-05 13:17:32 -0600

    updated user.jsx and moved the useEffect under the showHomePlanet

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,17 +4,6 @@ const User = ({ id }) => {
   const [user, setUser] = useState(null);
   const [homePlanet, setHomePlanet] = useState(null);
 
-  useEffect(() => {
-    fetch(`https://www.swapi.tech/api/people/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setUser(data.result.properties);
-      })
-      .catch((err) => {
-        console.error("Error fetching user data: ", err);
-      });
-  }, [id]);
-
   const showHomePlanet = () => {
     fetch(user.homeworld)
       .then((res) => res.json())
@@ -26,15 +15,29 @@ const User = ({ id }) => {
       });
   };
 
-  if (!user) {
-    return <p>Loading user data...</p>;
-  }
+  useEffect(() => {
+    fetch(`https://www.swapi.tech/api/people/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUser(data.result.properties);
+      })
+      .catch((err) => {
+        console.error("Error fetching user data: ", err);
+      });
+  }, [id]);
 
   return (
     <div>
-      <h1>{user.name}</h1>
-      <p>Home Planet: {homePlanet}</p>
-      <button onClick={showHomePlanet}>Show Home Planet</button>
+      {!user ? (
+        <p>Loading user data...</p>
+      ) : (
+        <>
+          <h1>{user.name}</h1>
+          <p>Home Planet: {homePlanet}</p>
+          <button onClick={showHomePlanet}>Show Home Planet</button>
+        </>
+      )}
     </div>
   );
 };
